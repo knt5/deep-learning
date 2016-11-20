74a273cc01de55fe5e03c63512df685ff7a7c3ed
knt5 <knt5@users.noreply.github.com>
2016-11-21 00:23:22 +0900

    Add 3.2.3

---
+++ chapter03/3.2.3/step.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+
+import numpy as np
+import matplotlib.pylab as plt
+
+def step(x):
+	return np.array(x > 0, dtype=np.int)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = step(x)
+
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1)  # y axis range
+plt.show()
