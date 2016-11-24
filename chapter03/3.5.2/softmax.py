bb8888267610f518e079b81cb1cd9e268021508b
knt5 <knt5@users.noreply.github.com>
2016-11-24 09:26:13 +0900

    Update softmax function

--- chapter03/3.5.2/softmax.py
+++ chapter03/3.5.2/softmax.py
@@ -4,9 +4,13 @@ import numpy as np
 
 # softmax function
 def softmax(a):
-	expA = np.exp(a)
+	c = np.max(a)
+	expA = np.exp(a - c)  # a - c : protected from overflow
 	sumExpA = np.sum(expA)
 	return expA / sumExpA
 
 a = np.array([0.3, 2.9, 4.0])
+print(softmax(a))
+
+a = np.array([1010, 1000, 990])
 print(softmax(a))
