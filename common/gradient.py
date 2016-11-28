02193d3f9c993bf6b56fb22748953fc40e3cdd5b
knt5 <knt5@users.noreply.github.com>
2016-11-29 00:51:01 +0900

    Add numerical gradient function

---
+++ common/gradient.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+
+import numpy as np
+
+def numericalGradientWithoutBatch(f, x):
+	h = 1e-4
+	gradient = np.zeros_like(x)  # same shape as x with 0 array
+	
+	for index in range(x.size):
+		t = x[index]
+		
+		# f(x+h)
+		x[index] = t + h
+		y1 = f(x)
+		
+		# f(x-h)
+		x[index] = t - h
+		y2 = f(x)
+		
+		# add to gradient
+		gradient[index] = (y1 - y2) / (2 * h)
+		
+		# revert
+		x[index] = t
+	
+	return gradient
+
+def numericalGradient(f, x):
+	if x.ndim == 1:
+		return numericalGradientWithoutBatch(f, x)
+	else:
+		gradient = np.zeros_like(x)
+		for index, t in enumerate(x):
+			gradient[index] = numericalGradientWithoutBatch(f, t)
+		return gradient
