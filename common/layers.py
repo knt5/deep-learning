967ebac4a8d59f69bd18117262c1f6a4cad8388a
knt5 <knt5@users.noreply.github.com>
2016-12-10 23:11:59 +0900

    Add Affine layer

--- common/layers.py
+++ common/layers.py
@@ -30,3 +30,24 @@ class Sigmoid:
 	def backward(self, dy):
 		dx = dy * (1.0 - self.y) * self.y
 		return dx
+
+class Affine:
+	def __init__(self, w, b):
+		self.w = w
+		self.b = b
+		self.x = None
+		
+		# derivation
+		self.dw = None
+		self.db = None
+	
+	def forward(self, x):
+		self.x = x
+		y = np.dot(self.x, self.w) + self.b
+		return y
+	
+	def backward(self, dy):
+		dx = np.dot(dy, self.w.T)
+		self.dw = np.dot(self.x.T, dy)
+		self.db = np.sum(dy, axis=0)
+		return dx
