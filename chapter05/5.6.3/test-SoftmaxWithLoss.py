4131e5086b2f23e805bfb650e74943884cd304cb
knt5 <knt5@users.noreply.github.com>
2016-12-13 23:05:24 +0900

    Fix to simulate batch

--- chapter05/5.6.3/test-SoftmaxWithLoss.py
+++ chapter05/5.6.3/test-SoftmaxWithLoss.py
@@ -8,16 +8,16 @@ from common.layers import SoftmaxWithLoss
 softmaxWithLoss = SoftmaxWithLoss()
 
 #---------------------------------------
-x = np.array([0.3, 0.2, 0.5])
-t = np.array([0, 1, 0])
+x = np.array([[0.3, 0.2, 0.5]])
+t = np.array([[0, 1, 0]])
 out = softmaxWithLoss.forward(x, t)
 print(out)
 dx = softmaxWithLoss.backward(1)
 print(dx)
 
 #---------------------------------------
-x = np.array([0.01, 0.99, 0.0])
-t = np.array([0, 1, 0])
+x = np.array([[0.01, 0.99, 0.0]])
+t = np.array([[0, 1, 0]])
 out = softmaxWithLoss.forward(x, t)
 print(out)
 dx = softmaxWithLoss.backward(1)
